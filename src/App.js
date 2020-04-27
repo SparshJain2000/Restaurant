1ae6d577c25f5a71df9d6a3fe6c3d28b3771c874
SparshJain2000 <jainsparsh0801@gmail.com>
2020-04-27 22:42:53 +0530

    adding components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Main from "./components/MainComponent";
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App' style={{ textAlign: "start" }}>
+                <Main />
+            </div>
+        );
+    }
+}
+
+export default App;
